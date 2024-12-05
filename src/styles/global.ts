6fb9e3bb8a6e09ad19585853e614015020089d8e
Silva <ronaldo.silva2@honeywell.com>
2024-12-05 16:28:35 -0300

    theme defined

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components'
+import { fonts } from './fonts'
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+:focus{
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['purple-dark']};
+}
+
+body{
+    background: ${props => props.theme.background};
+    color: ${props => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+    ${fonts.textM};
+      /* font-family: 'Baloo 2', sans-serif;
+      font-size: 1.5rem;
+      line-height: 130%;
+      font-weight: 800; */
+  }
+
+  button{
+    border: none;
+    cursor: pointer;
+  }
+`
